923810676379237b132d9796cf77e7ac94389f3a
shawn <dhryxy@gmail.com>
2020-12-10 17:54:55 +0800

    fix gov entry page

--- lib/pages/governance.dart
+++ lib/pages/governance.dart
@@ -18,17 +18,7 @@ class Gov extends StatelessWidget {
   Widget build(BuildContext context) {
     final dic = I18n.of(context).getDic(i18n_full_dic_kusama, 'gov');
     final dicCommon = I18n.of(context).getDic(i18n_full_dic_kusama, 'common');
-    if (plugin.sdk.api.connectedNode == null) {
-      return Container(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).size.width / 2),
-        child: Column(
-          children: [
-            CupertinoActivityIndicator(),
-            Text(dicCommon['node.connecting']),
-          ],
-        ),
-      );
-    }
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: SafeArea(
@@ -52,82 +42,93 @@ class Gov extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: ListView(
-                padding: EdgeInsets.all(16),
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: GestureDetector(
-                      child: EntryPageCard(
-                        dic['democracy'],
-                        dic['democracy.brief'],
-                        Icon(
-                          Icons.event_note,
-                          color: Colors.white,
-                          size: 56,
+              child: plugin.sdk.api.connectedNode == null
+                  ? Container(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.width / 2),
+                      child: Column(
+                        children: [
+                          CupertinoActivityIndicator(),
+                          Text(dicCommon['node.connecting']),
+                        ],
+                      ),
+                    )
+                  : ListView(
+                      padding: EdgeInsets.all(16),
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: GestureDetector(
+                            child: EntryPageCard(
+                              dic['democracy'],
+                              dic['democracy.brief'],
+                              Icon(
+                                Icons.event_note,
+                                color: Colors.white,
+                                size: 56,
+                              ),
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            onTap: () => Navigator.of(context)
+                                .pushNamed(DemocracyPage.route),
+                          ),
                         ),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(DemocracyPage.route),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: GestureDetector(
-                      child: EntryPageCard(
-                        dic['council'],
-                        dic['council.brief'],
-                        Icon(
-                          Icons.people_outline,
-                          color: Colors.white,
-                          size: 56,
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: GestureDetector(
+                            child: EntryPageCard(
+                              dic['council'],
+                              dic['council.brief'],
+                              Icon(
+                                Icons.people_outline,
+                                color: Colors.white,
+                                size: 56,
+                              ),
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            onTap: () => Navigator.of(context)
+                                .pushNamed(CouncilPage.route),
+                          ),
                         ),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(CouncilPage.route),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: GestureDetector(
-                      child: EntryPageCard(
-                        dic['treasury'],
-                        dic['treasury.brief'],
-                        Icon(
-                          Icons.account_balance,
-                          color: Colors.white,
-                          size: 56,
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: GestureDetector(
+                            child: EntryPageCard(
+                              dic['treasury'],
+                              dic['treasury.brief'],
+                              Icon(
+                                Icons.account_balance,
+                                color: Colors.white,
+                                size: 56,
+                              ),
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            onTap: () => Navigator.of(context)
+                                .pushNamed(TreasuryPage.route),
+                          ),
                         ),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(TreasuryPage.route),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: GestureDetector(
-                      child: EntryPageCard(
-                        'Polkassembly',
-                        dic['polkassembly'],
-                        Image.asset(
-                          'packages/polkawallet_plugin_kusama/assets/images/public/polkassembly.png',
-                          width: 56,
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: GestureDetector(
+                            child: EntryPageCard(
+                              'Polkassembly',
+                              dic['polkassembly'],
+                              Image.asset(
+                                'packages/polkawallet_plugin_kusama/assets/images/public/polkassembly.png',
+                                width: 56,
+                              ),
+                              color: Colors.black87,
+                            ),
+                            onTap: () => Navigator.of(context).pushNamed(
+                              DAppWrapperPage.route,
+                              arguments:
+                                  // 'https://${plugin.basic.name}.polkassembly.io/',
+                                  "https://apps.acala.network/#/loan",
+                            ),
+                          ),
                         ),
-                        color: Colors.black87,
-                      ),
-                      onTap: () => Navigator.of(context).pushNamed(
-                        DAppWrapperPage.route,
-                        arguments:
-                            // 'https://${plugin.basic.name}.polkassembly.io/',
-                            "https://apps.acala.network/#/loan",
-                      ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
             )
           ],
         ),
