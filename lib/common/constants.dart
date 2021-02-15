a625bbc2aae3c9841b5a5c9e728d764fcc76f13c
James Truong <jamestruong.consult@gmail.com>
2021-02-16 04:57:55 +1100

    update ChainX node list

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -5,11 +5,6 @@ const int SECONDS_OF_YEAR = 365 * 24 * 60 * 60; // seconds of one year
 
 const node_list_chainx = [
   {
-    'name': 'ChainX (Chinese Node)',
-    'ss58': 2,
-    'endpoint': 'wss://mainnet.spiderx.pro/ws',
-  },
-  {
     'name': 'ChainX (Overseas Node)',
     'ss58': 2,
     'endpoint': 'wss://mainnet.chainx.org/ws',
@@ -20,34 +15,6 @@ const node_list_chainx = [
     'endpoint': 'wss://chainx.elara.patract.io/',
   },
 ];
-const node_list_polkadot = [
-  {
-    'name': 'Polkadot (Live, hosted by PatractLabs)',
-    'ss58': 0,
-    'endpoint': 'wss://polkadot.elara.patract.io',
-  },
-  {
-    'name': 'Polkadot (Live, hosted by Polkawallet CN)',
-    'ss58': 0,
-    'endpoint': 'wss://polkadot-1.polkawallet.io:9944',
-  },
-  {
-    'name': 'Polkadot (Live, hosted by Polkawallet EU)',
-    'ss58': 0,
-    'endpoint': 'wss://polkadot-2.polkawallet.io',
-  },
-  {
-    'name': 'Polkadot (Live, hosted by Parity)',
-    'ss58': 0,
-    'endpoint': 'wss://rpc.polkadot.io',
-  },
-  {
-    'name': 'Polkadot (Live, hosted by onfinality)',
-    'ss58': 0,
-    'endpoint': 'wss://polkadot.api.onfinality.io/public-ws',
-  },
-];
-
 const home_nav_items = ['staking', 'governance'];
 
 const MaterialColor chainx_yellow = const MaterialColor(
