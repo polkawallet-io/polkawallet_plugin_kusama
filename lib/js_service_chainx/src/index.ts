18730010811fdb0aff7fb605413f95c60b2be3b8
James Truong <jamestruong.consult@gmail.com>
2021-02-23 09:24:04 +1100

    js_service_chainx update

--- lib/js_service_chainx/src/index.ts
+++ lib/js_service_chainx/src/index.ts
@@ -1,52 +1,49 @@
-import "@babel/polyfill";
-import { options, WsProvider, LaminarApi } from "@laminar/api";
-import { ApiPromise } from "@polkadot/api";
-import { subscribeMessage, getNetworkConst, getNetworkProperties } from "./service/setting";
-import { genLinks } from "./utils/config/config";
-import keyring from "./service/keyring";
-import account from "./service/account";
-import laminar from "./service/laminar";
+import "@babel/polyfill"
+import { ApiPromise } from "@polkadot/api"
+import { WsProvider } from "@polkadot/rpc-provider"
+import { options } from "@chainx-v2/api"
+import { subscribeMessage, getNetworkConst, getNetworkProperties } from "./service/setting"
+import { genLinks } from "./utils/config/config"
+import keyring from "./service/keyring"
+import account from "./service/account"
+import laminar from "./service/laminar"
 
 // send message to JSChannel: PolkaWallet
 function send(path: string, data: any) {
   if (window.location.href === "about:blank") {
-    PolkaWallet.postMessage(JSON.stringify({ path, data }));
+    PolkaWallet.postMessage(JSON.stringify({ path, data }))
   } else {
-    console.log(path, data);
+    console.log(path, data)
   }
 }
-send("log", "laminar main js loaded");
-(<any>window).send = send;
+send("log", "chainx main js loaded")
+;(<any>window).send = send
 
 async function connect(nodes: string[]) {
   return new Promise(async (resolve, reject) => {
-    const provider = new WsProvider(nodes);
+    const wsProvider = new WsProvider(nodes)
     try {
-      const laminarApi = new LaminarApi({ provider });
-      const res = new ApiPromise(options({ provider }));
-      await laminarApi.isReady();
-      await res.isReady;
-      (<any>window).laminarApi = laminarApi;
-      (<any>window).api = res;
-      const url = nodes[(<any>res)._options.provider.__private_15_endpointIndex];
-      send("log", `${url} wss connected success`);
-      resolve(url);
+      const res = await ApiPromise.create(options({ provider: wsProvider }))
+      ;(<any>window).api = res
+      send("log", res.genesisHash.toHuman())
+      await res.isReady
+      send("log", `wss connected success`)
+      resolve(true)
     } catch (err) {
-      send("log", `connect failed`);
-      provider.disconnect();
-      resolve(null);
+      send("log", `connect failed`)
+      wsProvider.disconnect()
+      resolve(null)
     }
-  });
+  })
 }
 
-(<any>window).settings = {
+;(<any>window).settings = {
   connect,
   getNetworkConst,
   getNetworkProperties,
   subscribeMessage,
   genLinks,
-};
-
-(<any>window).keyring = keyring;
-(<any>window).account = account;
-(<any>window).laminar = laminar;
+}
+;(<any>window).keyring = keyring
+;(<any>window).account = account
+;(<any>window).laminar = laminar
