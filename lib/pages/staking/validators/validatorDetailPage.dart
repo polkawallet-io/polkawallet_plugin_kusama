2265e44fee33e8edfd3413b3789d6291caf9c2fd
James Truong <jamestruong.consult@gmail.com>
2021-03-01 09:34:53 +1100

    validator detail page

--- lib/pages/staking/validators/validatorDetailPage.dart
+++ lib/pages/staking/validators/validatorDetailPage.dart
@@ -4,10 +4,10 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:polkawallet_plugin_chainx/common/components/infoItem.dart';
 import 'package:polkawallet_plugin_chainx/polkawallet_plugin_chainx.dart';
 import 'package:polkawallet_plugin_chainx/store/staking/types/validatorData.dart';
+import 'package:polkawallet_plugin_chainx/pages/staking/validators/accountInfo.dart';
 import 'package:polkawallet_plugin_chainx/utils/i18n/index.dart';
 import 'package:polkawallet_sdk/storage/keyring.dart';
 import 'package:polkawallet_sdk/utils/i18n.dart';
-import 'package:polkawallet_ui/components/accountInfo.dart';
 import 'package:polkawallet_ui/components/addressIcon.dart';
 import 'package:polkawallet_ui/components/borderedTitle.dart';
 import 'package:polkawallet_ui/components/roundedCard.dart';
@@ -37,88 +37,50 @@ class ValidatorDetailPage extends StatelessWidget {
               centerTitle: true,
             ),
             body: SafeArea(
-
-                // child: ListView.builder(
-                //   itemCount: 2 + (detail.isElected ? detail.nominators.length : plugin.store.staking.nominationsMap[detail.accountId]?.length ?? 0),
-                //   itemBuilder: (_, i) {
-                //     if (i == 0) {
-                //       return RoundedCard(
-                //         margin: EdgeInsets.all(16),
-                //         child: Column(
-                //           children: <Widget>[
-                //             AccountInfo(
-                //               network: plugin.basic.name,
-                //               accInfo: accInfo,
-                //               address: detail.accountId,
-                //               icon: accIcon,
-                //             ),
-                //             Divider(),
-                //             Padding(
-                //               padding: EdgeInsets.only(top: 16, left: 24),
-                //               child: Row(
-                //                 children: <Widget>[
-                //                   InfoItem(
-                //                     title: dicStaking['stake.own'],
-                //                     content: Fmt.token(detail.bondOwn, decimals),
-                //                   ),
-                //                   InfoItem(
-                //                     title: dicStaking['stake.other'],
-                //                     content: Fmt.token(detail.bondOther, decimals),
-                //                   ),
-                //                 ],
-                //               ),
-                //             ),
-                //             Padding(
-                //               padding: EdgeInsets.only(top: 16, left: 24, bottom: 24),
-                //               child: Row(
-                //                 children: <Widget>[
-                //                   InfoItem(
-                //                     title: dicStaking['commission'],
-                //                     content: detail.commission,
-                //                   ),
-                //                   InfoItem(
-                //                     title: dicStaking['reward'],
-                //                     content: '${detail.stakedReturnCmp.toStringAsFixed(2)}%',
-                //                   ),
-                //                 ],
-                //               ),
-                //             ),
-                //           ],
-                //         ),
-                //       );
-                //     }
-                //     if (i == 1) {
-                //       final addresses = detail.isElected ? detail.nominators.map((e) => e['who']).toList() : plugin.store.staking.nominationsMap[detail.accountId];
-                //       return Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: [
-                //           Padding(
-                //             padding: EdgeInsets.only(left: 16, top: 16),
-                //             child: BorderedTitle(
-                //               title: dicStaking['nominators'],
-                //             ),
-                //           ),
-                //           FutureBuilder(future: plugin.service.gov.updateIconsAndIndices(addresses), builder: (_, __) => Container()),
-                //         ],
-                //       );
-                //     }
-                //     if (detail.isElected) {
-                //       final item = detail.nominators[i - 2];
-                //       return ListTile(
-                //         leading: AddressIcon(item['who'], size: 32, svg: plugin.store.accounts.addressIconsMap[item['who']]),
-                //         title: UI.accountDisplayName(item['who'], plugin.store.accounts.addressIndexMap[item['who']]),
-                //         trailing: Text('${Fmt.balance(item['value'].toString(), plugin.networkState.tokenDecimals)} ${plugin.networkState.tokenSymbol}'),
-                //       );
-                //     } else {
-                //       final address = plugin.store.staking.nominationsMap[detail.accountId][i - 2];
-                //       return ListTile(
-                //         leading: AddressIcon(address, svg: plugin.store.accounts.addressIconsMap[address]),
-                //         title: UI.accountDisplayName(address, plugin.store.accounts.addressIndexMap[address]),
-                //       );
-                //     }
-                //   },
-                // ),
-                ),
+                child: RoundedCard(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                children: <Widget>[
+                  AccountInfo(
+                    network: plugin.basic.name,
+                    accInfo: accInfo,
+                    address: detail.accountId,
+                    icon: accIcon,
+                  ),
+                  Divider(),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16, left: 24),
+                    child: Row(
+                      children: <Widget>[
+                        InfoItem(
+                          title: dicStaking['overview.all'],
+                          content: detail.totalNominationFmt,
+                        ),
+                        InfoItem(
+                          title: dicStaking['overview.own'],
+                          content: detail.selfBondedFmt,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16, left: 24, bottom: 24),
+                    child: Row(
+                      children: <Widget>[
+                        InfoItem(
+                          title: dicStaking['overview.pots'],
+                          content: detail.rewardPotBalanceFmt,
+                        ),
+                        // InfoItem(
+                        //   title: dicStaking['reward'],
+                        //   content: '${detail.stakedReturnCmp.toStringAsFixed(2)}%',
+                        // ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
           );
         },
       );
