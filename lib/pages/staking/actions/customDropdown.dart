ce569ffe3bdd79e0c46088f35495e853040ddd33
James Truong <jamestruong.consult@gmail.com>
2021-04-08 06:20:57 +1000

    give auto height for dropdown

--- lib/pages/staking/actions/customDropdown.dart
+++ lib/pages/staking/actions/customDropdown.dart
@@ -43,7 +43,6 @@ class CustomDropdown<T> extends StatelessWidget {
             child: DropdownButtonHideUnderline(
               child: DropdownButton(
                 isExpanded: true,
-                itemHeight: 50.0,
                 style: TextStyle(fontSize: 15.0, color: isEnabled ? Colors.black : Colors.grey[700]),
                 items: dropdownMenuItemList,
                 onChanged: onChanged,
