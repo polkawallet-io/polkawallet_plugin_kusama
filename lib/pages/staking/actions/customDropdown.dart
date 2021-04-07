28127ff3ce5d401247bbdcb48386ff3ad9e9e52b
James Truong <jamestruong.consult@gmail.com>
2021-04-08 06:22:32 +1000

    dropdown button style

--- lib/pages/staking/actions/customDropdown.dart
+++ lib/pages/staking/actions/customDropdown.dart
@@ -43,7 +43,8 @@ class CustomDropdown<T> extends StatelessWidget {
             child: DropdownButtonHideUnderline(
               child: DropdownButton(
                 isExpanded: true,
-                style: TextStyle(fontSize: 15.0, color: isEnabled ? Colors.black : Colors.grey[700]),
+                itemHeight: 40.0,
+                // style: TextStyle(fontSize: 15.0, color: isEnabled ? Colors.black : Colors.grey[700]),
                 items: dropdownMenuItemList,
                 onChanged: onChanged,
                 value: value,
